ed8aee8d3ec111bd420d47785da3b61221d7eeaa
Keith Bennett <keithrbennett@gmail.com>
2018-08-23 23:31:50 +0700

    Fix db migrations by removing commas.

--- db/migrate/20180823145105_create_countries.rb
+++ db/migrate/20180823145105_create_countries.rb
@@ -1,11 +1,11 @@
 class CreateCountries < ActiveRecord::Migration[5.2]
   def change
     create_table :countries do |t|
-      t.string, :oa_id
-      t.string, :code
-      t.string, :name
-      t.string, :continent
-      t.string, :wikipedia_link
+      t.string :oa_id
+      t.string :code
+      t.string :name
+      t.string :continent
+      t.string :wikipedia_link
       t.string :keywords
 
       t.timestamps
--- db/migrate/20180823145129_create_regions.rb
+++ db/migrate/20180823145129_create_regions.rb
@@ -1,13 +1,13 @@
 class CreateRegions < ActiveRecord::Migration[5.2]
   def change
     create_table :regions do |t|
-      t.string, :oa_id
-      t.string, :code
-      t.string, :local_code
-      t.string, :name
-      t.string, :continent
-      t.string, :iso_country
-      t.string, :wikipedia_link
+      t.string :oa_id
+      t.string :code
+      t.string :local_code
+      t.string :name
+      t.string :continent
+      t.string :iso_country
+      t.string :wikipedia_link
       t.string :keywords
 
       t.timestamps
