f45e73238f9e975da90705c4d4a9584c4a79c50f
Keith Bennett <keithrbennett@gmail.com>
2018-08-28 15:05:26 +0700

    Disable regions and countries resources; send root to airports#index. Style table with Bootstrap. Disable Edit and Delete table row links.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :airports
-  resources :regions
-  resources :countries
+
+  root to: 'airports#index'
+
+  # resources :regions
+  # resources :countries
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
