a04f6f99296c13a871e93e1db943ff4df1b6f1be
Keith Bennett <keithrbennett@gmail.com>
2018-08-30 20:22:53 +0700

    Add arrow icons to sortable table headers.

--- app/helpers/airports_helper.rb
+++ app/helpers/airports_helper.rb
@@ -3,9 +3,9 @@ module AirportsHelper
   def sort_link(column, title = nil)
     title ||= column.titleize
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-    icon = sort_direction == "asc" ? "glyphicon glyphicon-chevron-up" : "glyphicon glyphicon-chevron-down"
-    icon = column == sort_column ? icon : ""
-    link_to "#{title} <span class='#{icon}'></span>".html_safe, {column: column, direction: direction}
+    arrow = sort_direction == "asc" ? "⬆" : "⬇"
+    arrow = column == sort_column ? arrow : ""
+    link_to "#{title} #{arrow}".html_safe, {column: column, direction: direction}
   end
 
 end
