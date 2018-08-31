41f3fd4a6d58f47329fd115bec49477ccc892906
Keith Bennett <keithrbennett@gmail.com>
2018-08-31 21:37:56 +0700

    Make home and Wikipedia link columns sortable to easily see which records have missing data. Fix horizontal alignment of headings and data.

--- app/helpers/airports_helper.rb
+++ app/helpers/airports_helper.rb
@@ -1,9 +1,12 @@
 module AirportsHelper
 
+  UP_ARROW_CHAR   = "\u2b06"  #  "⬆"
+  DOWN_ARROW_CHAR = "\u2b07"  #  "⬇"
+
   def sort_link(column, title = nil)
     title ||= column.titleize
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-    arrow = sort_direction == "asc" ? "⬆" : "⬇"
+    arrow = sort_direction == "asc" ? UP_ARROW_CHAR : DOWN_ARROW_CHAR
     arrow = column == sort_column ? arrow : ""
     link_to "#{title} #{arrow}".html_safe, {column: column, direction: direction}
   end
