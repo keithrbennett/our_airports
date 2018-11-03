747b43dcbfb51dfc6de3b55e1972843c93848ca3
Keith Bennett <keithrbennett@gmail.com>
2018-11-03 16:04:08 -0400

    Fix failing tests: fix field name, disable tests for excluded routes, use null conditional.

--- test/controllers/airports_controller_test.rb
+++ test/controllers/airports_controller_test.rb
@@ -10,39 +10,39 @@ class AirportsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get new" do
-    get new_airport_url
-    assert_response :success
-  end
-
-  test "should create airport" do
-    assert_difference('Airport.count') do
-      post airports_url, params: { airport: { continent: @airport.continent, elevation_ft: @airport.elevation_ft, gps_code: @airport.gps_code, home_link: @airport.home_link, iata_code: @airport.iata_code, ident: @airport.ident, iso_country: @airport.iso_country, iso_region: @airport.iso_region, keywords: @airport.keywords, lat: @airport.lat, long: @airport.long, municipality: @airport.municipality, name: @airport.name, oa_id: @airport.oa_id, scheduled_service: @airport.scheduled_service, type: @airport.airport_type, wikipedia_link: @airport.wikipedia_link } }
-    end
-
-    assert_redirected_to airport_url(Airport.last)
-  end
+  # test "should get new" do
+  #   get new_airport_url
+  #   assert_response :success
+  # end
+  #
+  # test "should create airport" do
+  #   assert_difference('Airport.count') do
+  #     post airports_url, params: { airport: { continent: @airport.continent, elevation_ft: @airport.elevation_ft, gps_code: @airport.gps_code, home_link: @airport.home_link, iata_code: @airport.iata_code, ident: @airport.ident, iso_country: @airport.iso_country, iso_region: @airport.iso_region, keywords: @airport.keywords, lat: @airport.lat, long: @airport.long, municipality: @airport.municipality, name: @airport.name, oa_id: @airport.oa_id, scheduled_service: @airport.scheduled_service, type: @airport.airport_type, wikipedia_link: @airport.wikipedia_link } }
+  #   end
+  #
+  #   assert_redirected_to airport_url(Airport.last)
+  # end
 
   test "should show airport" do
     get airport_url(@airport)
     assert_response :success
   end
 
-  test "should get edit" do
-    get edit_airport_url(@airport)
-    assert_response :success
-  end
-
-  test "should update airport" do
-    patch airport_url(@airport), params: { airport: { continent: @airport.continent, elevation_ft: @airport.elevation_ft, gps_code: @airport.gps_code, home_link: @airport.home_link, iata_code: @airport.iata_code, ident: @airport.ident, iso_country: @airport.iso_country, iso_region: @airport.iso_region, keywords: @airport.keywords, lat: @airport.lat, long: @airport.long, municipality: @airport.municipality, name: @airport.name, oa_id: @airport.oa_id, scheduled_service: @airport.scheduled_service, type: @airport.airport_type, wikipedia_link: @airport.wikipedia_link } }
-    assert_redirected_to airport_url(@airport)
-  end
-
-  test "should destroy airport" do
-    assert_difference('Airport.count', -1) do
-      delete airport_url(@airport)
-    end
-
-    assert_redirected_to airports_url
-  end
+  # test "should get edit" do
+  #   get edit_airport_url(@airport)
+  #   assert_response :success
+  # end
+  #
+  # test "should update airport" do
+  #   patch airport_url(@airport), params: { airport: { continent: @airport.continent, elevation_ft: @airport.elevation_ft, gps_code: @airport.gps_code, home_link: @airport.home_link, iata_code: @airport.iata_code, ident: @airport.ident, iso_country: @airport.iso_country, iso_region: @airport.iso_region, keywords: @airport.keywords, lat: @airport.lat, long: @airport.long, municipality: @airport.municipality, name: @airport.name, oa_id: @airport.oa_id, scheduled_service: @airport.scheduled_service, type: @airport.airport_type, wikipedia_link: @airport.wikipedia_link } }
+  #   assert_redirected_to airport_url(@airport)
+  # end
+  #
+  # test "should destroy airport" do
+  #   assert_difference('Airport.count', -1) do
+  #     delete airport_url(@airport)
+  #   end
+  #
+  #   assert_redirected_to airports_url
+  # end
 end
--- test/controllers/regions_controller_test.rb
+++ test/controllers/regions_controller_test.rb
@@ -5,44 +5,44 @@ class RegionsControllerTest < ActionDispatch::IntegrationTest
     @region = regions(:one)
   end
 
-  test "should get index" do
-    get regions_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_region_url
-    assert_response :success
-  end
-
-  test "should create region" do
-    assert_difference('Region.count') do
-      post regions_url, params: { region: { code: @region.code, continent: @region.continent, iso_country: @region.iso_country, keywords: @region.keywords, local_code: @region.local_code, name: @region.name, oa_id: @region.oa_id, wikipedia_link: @region.wikipedia_link } }
-    end
-
-    assert_redirected_to region_url(Region.last)
-  end
-
-  test "should show region" do
-    get region_url(@region)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_region_url(@region)
-    assert_response :success
-  end
-
-  test "should update region" do
-    patch region_url(@region), params: { region: { code: @region.code, continent: @region.continent, iso_country: @region.iso_country, keywords: @region.keywords, local_code: @region.local_code, name: @region.name, oa_id: @region.oa_id, wikipedia_link: @region.wikipedia_link } }
-    assert_redirected_to region_url(@region)
-  end
-
-  test "should destroy region" do
-    assert_difference('Region.count', -1) do
-      delete region_url(@region)
-    end
-
-    assert_redirected_to regions_url
-  end
+  # test "should get index" do
+  #   get regions_url
+  #   assert_response :success
+  # end
+  #
+  # test "should get new" do
+  #   get new_region_url
+  #   assert_response :success
+  # end
+  #
+  # test "should create region" do
+  #   assert_difference('Region.count') do
+  #     post regions_url, params: { region: { code: @region.code, continent: @region.continent, iso_country: @region.iso_country, keywords: @region.keywords, local_code: @region.local_code, name: @region.name, oa_id: @region.oa_id, wikipedia_link: @region.wikipedia_link } }
+  #   end
+  #
+  #   assert_redirected_to region_url(Region.last)
+  # end
+  #
+  # test "should show region" do
+  #   get region_url(@region)
+  #   assert_response :success
+  # end
+  #
+  # test "should get edit" do
+  #   get edit_region_url(@region)
+  #   assert_response :success
+  # end
+  #
+  # test "should update region" do
+  #   patch region_url(@region), params: { region: { code: @region.code, continent: @region.continent, iso_country: @region.iso_country, keywords: @region.keywords, local_code: @region.local_code, name: @region.name, oa_id: @region.oa_id, wikipedia_link: @region.wikipedia_link } }
+  #   assert_redirected_to region_url(@region)
+  # end
+  #
+  # test "should destroy region" do
+  #   assert_difference('Region.count', -1) do
+  #     delete region_url(@region)
+  #   end
+  #
+  #   assert_redirected_to regions_url
+  # end
 end
