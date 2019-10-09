2a1b4c59c7db85dbadb92eaaee2bf9dff9a2ccab
Keith Bennett <keithrbennett@gmail.com>
2019-10-09 17:37:08 +0700

    #2 - Add script to automate the updating of seed data from our_airports.com.

---
+++ update_data.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+
+# update_data.rb
+#
+# Fetches CSV data from ourairports.com, converts it to JSON, then writes that to seed_data directory.
+# To complete the data update in the app, recreate the data base, e.g. `rails db:setup`
+
+require 'smarter_csv'
+require 'stringio'
+require 'yaml'
+
+
+DATA_SOURCE_URLS = {
+    'airports'  => 'https://ourairports.com/data/airports.csv',
+    'countries' => 'https://ourairports.com/data/countries.csv',
+    'regions'   => 'https://ourairports.com/data/regions.csv',
+}
+
+
+DATA_SOURCE_URLS.each do |filename_base, source_url|
+  puts "\nDownloading #{source_url}...\n\n"
+  csv_text = `curl #{source_url}`
+  hash_array = SmarterCSV.process(StringIO.new(csv_text))
+  yaml_text = hash_array.to_yaml
+
+  output_filespec = File.join('seed_data', "#{filename_base}.yaml")
+  File.write(output_filespec, yaml_text)
+  puts "\nWrote #{hash_array.size} records to #{output_filespec}.\n\n"
+end
+
+puts 'Done'
+
+
+
+
